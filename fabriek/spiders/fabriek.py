548b0467be2a20411c745c6018e4297730e27715
Eelco Aartsen <eelco@aesset.nl>
2020-06-07 13:47:21 +0200

    Sort fabriek.csv file into fabriek-sorted.csv file

--- fabriek/spiders/fabriek.py
+++ fabriek/spiders/fabriek.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from typing import Dict, List
 import csv
 import scrapy
@@ -22,17 +23,17 @@ class FabriekSpider(CrawlSpider):
     def parse(self, response):
         day_urls = response.xpath("//a[@class='day-selector__day']/@href").getall()
         for day_url in day_urls:
-            day = day_url[-10:] #yyyy-mm-dd
-            complete_url = self.start_urls[0]+day_url
+            day = day_url[-10:]  # yyyy-mm-dd
+            complete_url = self.start_urls[0] + day_url
             yield scrapy.Request(url=complete_url, callback=self.parse_day,
                                  priority=10,
                                  cb_kwargs=dict(day=day))
 
     def parse_day(self, response, day: str):
         self.logger.debug("PARSE_DAY: " + day)
-        movie_urls  : List[str]  = response.xpath("//div[@class='main-agenda-movie-info']/h4/a/@href").getall()
-        movie_times : List[str]  = response.xpath("//div[@class='main-agenda-movie-time']/a/text()").getall()
-        movie_titles: List[str]  = response.xpath("//div[@class='main-agenda-movie-info']/h4/a/text()").getall()
+        movie_urls: List[str] = response.xpath("//div[@class='main-agenda-movie-info']/h4/a/@href").getall()
+        movie_times: List[str] = response.xpath("//div[@class='main-agenda-movie-time']/a/text()").getall()
+        movie_titles: List[str] = response.xpath("//div[@class='main-agenda-movie-info']/h4/a/text()").getall()
         movie_tickets: List[str] = response.xpath("//a[@class='button ticket-button']/@href").getall()
         i: int = 0
         for movie_title in movie_titles:
@@ -48,15 +49,15 @@ class FabriekSpider(CrawlSpider):
 
     def parse_movie(self, response, title, day, time, ticket_url, movie_url):
         self.logger.debug("PARSE_MOVIE: " + title)
-        title: str          = response.xpath("//div[@class='hero-slide-content']/h1/text()").get()
-        language: str       = get_text_from_movie(response, "Gesproken taal:")
-        genres: str         = get_text_from_movie(response, "Genre:")
-        playing_time: str   = get_text_from_movie(response, "Speelduur:")
-        cast: str           = get_text_from_movie(response, "Cast:")
-        synopsis: str       = response.xpath("//p[@class='film__synopsis__intro']/strong/text()").get()
-        content_detail1  = response.xpath("//div[@class='film__content__details__left']/p[1]/text()").get()
-        content_detail2  = response.xpath("//div[@class='film__content__details__left']/p[2]/text()").get()
-        content_detail3  = response.xpath("//div[@class='film__content__details__left']/p[3]/text()").get()
+        title: str = response.xpath("//div[@class='hero-slide-content']/h1/text()").get()
+        language: str = get_text_from_movie(response, "Gesproken taal:")
+        genres: str = get_text_from_movie(response, "Genre:")
+        playing_time: str = get_text_from_movie(response, "Speelduur:")
+        cast: str = get_text_from_movie(response, "Cast:")
+        synopsis: str = response.xpath("//p[@class='film__synopsis__intro']/strong/text()").get()
+        content_detail1 = response.xpath("//div[@class='film__content__details__left']/p[1]/text()").get()
+        content_detail2 = response.xpath("//div[@class='film__content__details__left']/p[2]/text()").get()
+        content_detail3 = response.xpath("//div[@class='film__content__details__left']/p[3]/text()").get()
         content_detail = ""
         if content_detail1 is not None:
             content_detail += content_detail1
@@ -65,40 +66,59 @@ class FabriekSpider(CrawlSpider):
         if content_detail3 is not None:
             content_detail += content_detail3
 
-        yield{'datum':          day,
-              'tijd' :          time,
-              'titel':          title,
-              'taal':           language,
-              'genre':          genres,
-              'speelduur':      playing_time,
-              'cast':           cast,
-              'synopsis':       synopsis,
-              'beschrijving':   content_detail,
-              'ticket-url': ticket_url,
-              'film-url': movie_url
-              }
+        yield {'datum': day,
+               'tijd': time,
+               'titel': title,
+               'taal': language,
+               'genre': genres,
+               'speelduur': playing_time,
+               'cast': cast,
+               'synopsis': synopsis,
+               'beschrijving': content_detail,
+               'ticket-url': ticket_url,
+               'film-url': movie_url
+               }
+
+
+
+
+def delete_file(file_name):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+output_csv_file: str = "../../output/fabriek.csv"
+output_csv_file_sorted: str = "../../output/fabriek-sorted.csv"
+
+delete_file(output_csv_file)
+delete_file(output_csv_file_sorted)
+
 
 process = CrawlerProcess(settings={
     "FEEDS": {
-        "../../output/fabriek.csv": {"format": "csv"},
+        output_csv_file: {"format": "csv"},
     }
 })
 
 process.crawl(FabriekSpider)
-process.start() # the script will block here until the crawling is finished
+process.start()  # the script will block here until the crawling is finished
 
-#open and read the file
+# open and read the file
 
-# with open('fabriek.csv') as csv_file:
-#     movie_list: List[str]
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     line_count = 0
-#     for row in csv_reader:
-#         if line_count == 0:
-#             print(", ".join(row))
-#             line_count += 1
-#         else:
+with open(output_csv_file) as csv_file:
+    header_row: List[str]
+    movie_list: List[List[str]] = []
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            header_row = row
+            line_count += 1
+        else:
+            movie_list.append(row)
 
-
-
-
+movie_list.sort()
+output_file = open(output_csv_file_sorted, mode="w")
+output_file.write(",".join(header_row) + "\n")
+for row in movie_list:
+    output_file.write(",".join(row) + "\n")
+output_file.close()
